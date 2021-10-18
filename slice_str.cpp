8727cefec960dc5653c9e44c823a52414b8831f1
BorisovetsGleb <92142560+BorisovetsGleb@users.noreply.github.com>
2021-10-18 12:49:11 +0300

    Add files via upload

--- slice_str.cpp
+++ slice_str.cpp
@@ -5,6 +5,8 @@ string itc_slice_str(string str, int start, int endd)
     long long len = itc_len(str);
     int i = start - 1;
     string out;
+    if(start > endd)
+        return "-1";
     while (i <= endd)
     {
         if(i < len)
