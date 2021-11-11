86f051c564ce7a481bb03b8d03ae1a1a0d2263ef
BorisovetsGleb <92142560+BorisovetsGleb@users.noreply.github.com>
2021-11-11 09:43:54 +0300

    Add files via upload

--- cmp_str.cpp
+++ cmp_str.cpp
@@ -7,28 +7,22 @@ string itc_cmp_str(string str1, string str2, int i)
     if(i < 0)
         return "-1";
     string out = "";
-    //i = i - 1;
     int b = 0;
     int c = 0;
-    //str2 = itc_slice_str(str2, 0, len2 - 2);
-    //cout <<"len1 = " <<len <<" len2 = " <<len2 <<endl;
     while (str1[b] != '\0')
     {
-        //cout <<b <<" " <<c;
         if(b >= i && c < len2)
         {
             out = out + str2[c];
             c ++;
-            //cout <<" Use 2: " <<c;
         }
         else
         {
             if(b < i || b >= len2)
                 out = out + str1[b];
+            b ++;
         }
-        b ++;
-        //cout <<"  ";
+        //b ++;
     }
-    //cout <<"OUT = " <<out <<"  ";
     return out;
 }
