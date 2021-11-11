68c834a8667b83d42a1927e882cb4c08cb261d72
BorisovetsGleb <92142560+BorisovetsGleb@users.noreply.github.com>
2021-11-11 09:08:58 +0300

    Add files via upload

--- str_easy.h
+++ str_easy.h
@@ -18,5 +18,6 @@ string itc_slice_str(string str, int a, int b);
 bool itc_equal_reverse(string str);
 string itc_cmp_str(string str1, string str2, int i);
 int itc_find_str(string str1, string str2);string itc_three_str(string str1, string str2, string str3);
+string itc_three_str(string str1, string str2, string str3);
 int itc_max_char_on_end(string str);
 #endif // STR_EASY_H_INCLUDED
